3eb891d70556608b5e91adbb7cf3556b5467cb64
mepripri <mepripri0712@gmail.com>
2024-07-31 15:35:35 -0400

    Changed highlight header

--- src/components/Highlights/Highlights.jsx
+++ src/components/Highlights/Highlights.jsx
@@ -27,11 +27,11 @@ function Highlights() {
         <div className="highlight-menu-list">
           {highlight_list.map((item, index) => (
             <div key={index} className="highlight-menu-list-item">
-              <img 
-                className="active" 
-                src={item.highlight_image} 
-                alt={item.highlight_name} 
-                onClick={() => openModal(item)} 
+              <img
+                className="active"
+                src={item.highlight_image}
+                alt={item.highlight_name}
+                onClick={() => openModal(item)}
               />
               <p>{item.highlight_name}</p>
             </div>
@@ -39,10 +39,12 @@ function Highlights() {
         </div>
       </div>
       <div className="story-highlights">
-        <h2>Story Highlights</h2>
         {storyHighlights.map((highlight, index) => (
           <div key={index} className="story-highlight-item">
-            <img src={highlight.highlight_image} alt={highlight.highlight_name} />
+            <img
+              src={highlight.highlight_image}
+              alt={highlight.highlight_name}
+            />
             <p>{highlight.highlight_name}</p>
           </div>
         ))}
@@ -50,7 +52,10 @@ function Highlights() {
       {isModalOpen && currentHighlight && (
         <div className="modal" onClick={closeModal}>
           <div className="modal-content" onClick={(e) => e.stopPropagation()}>
-            <img src={currentHighlight.highlight_image} alt={currentHighlight.highlight_name} />
+            <img
+              src={currentHighlight.highlight_image}
+              alt={currentHighlight.highlight_name}
+            />
             <p>{currentHighlight.highlight_name}</p>
           </div>
         </div>
