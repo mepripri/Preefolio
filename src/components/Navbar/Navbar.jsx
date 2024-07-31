7e2740c483fe2d0bcda5561b39a4c2d65f49bcb5
mepripri <mepripri0712@gmail.com>
2024-07-31 17:25:14 -0400

    Changed body 5

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,16 +5,9 @@ import { assets } from "../../assets/assets";
 function Navbar() {
   return (
     <div className="navbar">
-      <img className="navbar-img" src={assets.settings} alt="" />
       <div className="inner-navbar">
         <p>mepripri</p>
         <img src={assets.chevron} alt="" />
-      </div>
-      <div className="navbar-dot">
-        <img className="navbar-img" src={assets.threads} alt="" />
-        <div className="dot">
-          <p>9+</p>
-        </div>
       </div>
     </div>
   );
